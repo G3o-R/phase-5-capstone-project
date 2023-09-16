4f4a6e352261a909f03909432e5c8b794a5b580b
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-16 12:46:49 -0700

    added custom message for the email in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_many :posts
     has_many :comments, through: :posts
   
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+    validates :email, presence: true, uniqueness: { message: "An account with this email already exists" }, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
     validates :user_name, presence: true, uniqueness: true
     validates :password_digest, length: { minimum: 8 }, format: { with: /\A(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+\z/ }
   end
