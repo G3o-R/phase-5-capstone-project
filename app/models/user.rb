81e47e0ab82b94684d8e91ea33b39266cafac5fe
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-16 12:43:58 -0700

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
-
+  
     has_many :posts
     has_many :comments, through: :posts
-
-end
+  
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+    validates :user_name, presence: true, uniqueness: true
+    validates :password_digest, length: { minimum: 8 }, format: { with: /\A(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+\z/ }
+  end
+  
