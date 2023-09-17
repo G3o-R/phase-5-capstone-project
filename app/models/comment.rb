2a504f84890460c3d2f5ab8be8ad30446e1293ac
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-17 10:26:09 -0700

    added validations to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   has_many :comment_replies
+
+  validates :user_id, presence: :true
+  validates :post_id, presence: { message: "Sorry but this post no longer exists" }
+  validates :comment, presence: true
 end
