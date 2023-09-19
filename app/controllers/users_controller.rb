57a6d274c921cbb8a2efbbd4cf8f0230a11f7967
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-19 11:34:49 -0700

    modified create mthod in user to include sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     skip_before_action :authorize, only: [:create, :index, :show]
     def create 
         user = User.create!(user_params)
+        session[:user_id] = user.id
         render json: user, status: :created
     end
     def index 
