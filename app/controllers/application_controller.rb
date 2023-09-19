145d3570b00227a3d658ffde56e0d81f548450f3
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-19 11:18:06 -0700

    included authorize method into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
-include ActionController::Cookies
+    before_action :authorize
+    include ActionController::Cookies
+
+    private
+    def authorize
+        @current_user = User.find_by(id: session[:user_id])
+        render json: { ["Not authorized"] }, status: :unauthorized unless @current_user
+    end
 end
