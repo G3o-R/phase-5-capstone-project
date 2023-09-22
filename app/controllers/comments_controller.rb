2eb1ce996aa8fc6c0e0690ff42cc1d351a59b580
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-22 10:02:35 -0700

    added create to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,22 @@
 class CommentsController < ApplicationController
-    skip_before_action :authorize, only: :index
+    skip_before_action :authorize, only: [:index, :create]
     # don't forget to delete index method in comments controller users
     # can read comments as nested data for posts
     # same for comment_replies
     def index
         render json: Comment.all
+    end
+
+    def create 
+        # comment = User.comments.create!(comment_params)
+        comment = Comment.create!(comment_params)
+        render json: comment, status: :created
+    end
+
+    private
+
+    def comment_params
+        # don't forget to delete :user_id from prarams this is just for postman
+        params.permit(:post_id, :comment, :user_id)
     end
 end
