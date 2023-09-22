1613f8a57ec92cf58b8fb53d2769bb2fe2de8ae6
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-22 00:00:15 -0700

    building posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
 class PostsController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+    def index
+        posts = Post.all
+        render json: posts
+    end
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :image, :likes, :description, :comments
-  belongs_to :user
-  has_many :comments
+  attributes :id, :image, :likes, :description, :user, :comments
+  # has_many :comments
+  
+  def user
+    object.user.username
+  end
+
 end
