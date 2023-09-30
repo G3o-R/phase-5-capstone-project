0477c68523c65d67c270741a7c915e3210678aa1
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-29 18:16:54 -0700

    building and styling ProfilePage

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,16 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { ProfilePageContainer } from "../styles/ProfilePageStyles"; // Import the updated style
+import {
+  ProfilePageContainer,
+  PostRow,
+  PostContainer,
+  PostsContainer
+} from "../styles/ProfilePageStyles"; // Import the updated styled components
+
+// ... rest of your code ...
 
 function ProfilePage() {
-  const { user, loading } = useSelector((state) => state.user);
-  console.log(user)
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
+
+  const displayUserPosts = user.user_posts.map((post, index) => (
+    <PostContainer key={post.id}>
+      <Link to={`/post/${post.id}`}>
+        <img src={post.image} alt="content" />
+      </Link>
+    </PostContainer>
+  ));
+
+  // Calculate the number of empty divs needed to fill the row
+  const numEmptyDivs = 3 - (displayUserPosts.length % 3);
+
+  // Create an array to hold the empty divs
+  const emptyDivs = Array(numEmptyDivs)
+    .fill(null)
+    .map((_, index) => <PostContainer key={`empty-${index}`} />);
+
+  // Concatenate the empty divs to the posts
+  const postsWithEmptyDivs = displayUserPosts.concat(emptyDivs);
+
+  const rows = [];
+  for (let i = 0; i < postsWithEmptyDivs.length; i += 3) {
+    const row = postsWithEmptyDivs.slice(i, i + 3);
+    rows.push(row);
+  }
+
+  const renderedRows = rows.map((row, index) => (
+    <PostRow key={index}>{row}</PostRow>
+  ));
 
   return (
     <ProfilePageContainer>
-      <div name="profile-page">
         <h1>{user.username}</h1>
-      </div>
+        <PostsContainer>{renderedRows}</PostsContainer>
     </ProfilePageContainer>
   );
 }
--- client/src/styles/ProfilePageStyles.js
+++ client/src/styles/ProfilePageStyles.js
@@ -1,6 +1,40 @@
 import styled from "styled-components";
 
 export const ProfilePageContainer = styled.div`
-  margin-left: 336px;
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px; /* Adjust the gap between elements as needed */
+
+  h1 {
+    font-size: 24px;
+    margin-bottom: 16px;
+  }
 `;
+
+export const PostRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px; /* Adjust the gap between posts as needed */
+  align-items: center;
+`;
+
+export const PostsContainer = styled.div`
+  display: flex;
+  flex-direction:column;
+`
+
+export const PostContainer = styled.div`
+  width: 100%;
+  max-width: 300px; /* Adjust the maximum width as needed */
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
