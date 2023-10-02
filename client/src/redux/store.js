52da641e42986e500f3d28fe1740d07a7ac946b9
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-10-02 10:53:26 -0700

    updated store *note back tracked the biography and profile picture migration

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,11 +2,13 @@
 import { configureStore} from "@reduxjs/toolkit";
 import allUsersReducer from "./features/allUsersSlice";
 import userReducer from "./features/userSlice"
+import allPostsReducer from "./features/allPostsSlice";
 
 export  default configureStore({
     reducer: {
         allUsers: allUsersReducer,
-        user: userReducer
+        user: userReducer,
+        allPosts: allPostsReducer
     },
     middleware: (getDefaultMiddleware => getDefaultMiddleware({
         serializableCheck: false
