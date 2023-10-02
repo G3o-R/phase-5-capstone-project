070ab746508233fbab41326a59f31e240a1520fb
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-10-02 12:59:57 -0700

    building postCards in Home

--- client/src/component/PostCard.js
+++ client/src/component/PostCard.js
@@ -1,9 +1,26 @@
+import { ReactComponent as LikeSVG } from "../images/Like.svg"
+import { ReactComponent as CommentSVG } from "../images/Comment.svg"
 
-
-function PostCard(){
+function PostCard({post}){
+    const {comments, description, image, likes, user} = post
     return (
         <div name="post-card">
-            
+            <h3 name="users-name">{user}</h3>
+            <div name="post-container">
+                <img src={image} alt={`${user}'s post`} name="post"/>
+            </div>
+            <div name="bottom-post-section">
+                <div name="like-and-comment-btns">
+                    <button onClick={()=>console.log("click like btn")}><LikeSVG/></button>
+                    <button onClick={()=>console.log("click comment btn")}><CommentSVG/></button>
+                </div>
+            <h4 name="likes">{likes}</h4>
+            <h4 name="decription">{description}</h4>
+            <div name="see-comments-button">
+                <p onClick={()=>console.log("display comments")}>View all {CommentSVG.length} comments</p>
+                <p onClick={()=>console.log("display comments")}>Add a comment...</p>
+            </div>
+            </div>
         </div>
     )
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom"
 import { getPosts } from "../redux/features/allPostsSlice"
 import { useEffect } from "react"
 
+import PostCard from "../component/PostCard"
+
 function Home(){
     const {posts} = useSelector((state) => state.allPosts)
     const dispatch = useDispatch()
@@ -11,12 +13,14 @@ function Home(){
         dispatch(getPosts())
 
     },[])
-    console.log(posts)
+    // console.log(posts[0].comments)
+    const postsArray = posts.map((post)=> <PostCard post={post} key={post.id}/>)
 
     return (
 
         <div>
             Home is here
+            {postsArray}
         </div>
     )
 }
