baa256699bedd8a1bdabffb788ae930ed7288226
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-10-02 11:39:12 -0700

    Home has access to posts data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,17 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
+import { getPosts } from "../redux/features/allPostsSlice"
+import { useEffect } from "react"
 
 function Home(){
-    const {user} = useSelector((state) => state.user)
+    const {posts} = useSelector((state) => state.allPosts)
+    const dispatch = useDispatch()
     const naviagte = useNavigate()
+    useEffect(()=>{
+        dispatch(getPosts())
+
+    },[])
+    console.log(posts)
 
     return (
 
