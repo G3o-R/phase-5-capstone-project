372ce157816f873a5557a359298727859ac18eea
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-10-18 15:33:43 -0700

    errors are displayed from HomePage as well now

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,32 @@
 import { useSelector, useDispatch } from "react-redux"
 import { getPosts } from "../redux/features/allPostsSlice"
+import { clearErrors } from "../redux/features/commentsSlice"
 import { useEffect } from "react"
 import {
      HomePage,
      PostsContainer,
-    
     } from "../styles/HomeStyles"
 
 import PostCard from "../component/PostCard"
+import DisplayErrors from "../component/DisplayErrors"
 
 function Home(){
     const {posts} = useSelector((state) => state.allPosts)
+    const {errors} = useSelector((state) => state.comments)
+    console.log("test")
     const dispatch = useDispatch()
     useEffect(()=>{
         dispatch(getPosts())
 
     },[dispatch])
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          dispatch(clearErrors());
+        }, 5000);
+    
+        return () => clearTimeout(timer);
+      }, [errors, dispatch]);
 
     const postsArray = posts.map((post)=> <PostCard post={post} key={post.id}/>)
 
@@ -25,6 +36,8 @@ function Home(){
         <PostsContainer>
         {postsArray}
         </PostsContainer>
+    {errors.length > 0 ? <DisplayErrors errors={errors}/> : null}
+
         </HomePage>
     )
 }
